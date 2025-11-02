f953fe56b00eec76ecd1d2e77592122f57224bde
sivaperumal84 <siva.perumal84@gmail.com>
2025-11-02 13:06:09 -0800

    Refactor Supabase client initialization
    Removed hardcoded Supabase client initialization.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,11 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
-
-export const supabase = createClient(
-  supabaseUrl || 'https://afqlrqpsvvfzkdacqwfx.supabase.co',
-  supabaseAnonKey || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmcWxycXBzdnZmemtkYWNxd2Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwOTQyNzksImV4cCI6MjA3NzY3MDI3OX0.vCAdP630uwvaeK6ZNISMo3LZC72ea-AmlKbDPL8asH4'
-)
+ 
 
 export type Book = {
   id: string
